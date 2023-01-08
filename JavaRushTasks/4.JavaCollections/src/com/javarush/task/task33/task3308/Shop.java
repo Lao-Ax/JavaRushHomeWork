ef09de30ab706c9acd85197d151b973068490a83
Alex Plekhov <aplekhov@wiley.com>
2023-01-08 19:39:49 +0300

    Comment old code for serialization

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3308/Shop.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3308/Shop.java
@@ -1,50 +1,49 @@
 package com.javarush.task.task33.task3308;
 
-import com.sun.xml.internal.txw2.annotation.XmlCDATA;
-import org.omg.CORBA.INTERNAL;
-
-import javax.xml.bind.annotation.*;
-import javax.xml.bind.annotation.adapters.XmlAdapter;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+//import com.sun.xml.internal.txw2.annotation.XmlCDATA;
+//import org.omg.CORBA.INTERNAL;
+//
+//import javax.xml.bind.annotation.*;
+//import javax.xml.bind.annotation.adapters.XmlAdapter;
+//import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.util.List;
 
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
+//@XmlRootElement
+//@XmlAccessorType(XmlAccessType.FIELD)
 public class Shop {
-    public Goods goods;
-    public Integer count;
-    public double profit;
-    @XmlJavaTypeAdapter(AdapterCDATA.class)
-    @XmlElement
-    public String[] secretData;
-
-    public static class Goods {
-        public List<String> names;
-    }
-
-    @XmlCDATA
-    public int getCount() {
-        return count;
-    }
-
-    @XmlCDATA
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-    public static class AdapterCDATA extends XmlAdapter<String, String> {
-
-        @Override
-        public String marshal(String arg0) throws Exception {
-            if (arg0.isEmpty()) return "";
-            return "<![CDATA[" + arg0 + "]]>";
-        }
-
-        @Override
-        public String unmarshal(String arg0) throws Exception {
-            return arg0;
-        }
-
-    }
-
+//    public Goods goods;
+//    public Integer count;
+//    public double profit;
+//    @XmlJavaTypeAdapter(AdapterCDATA.class)
+//    @XmlElement
+//    public String[] secretData;
+//
+//    public static class Goods {
+//        public List<String> names;
+//    }
+//
+//    @XmlCDATA
+//    public int getCount() {
+//        return count;
+//    }
+//
+//    @XmlCDATA
+//    public void setCount(int count) {
+//        this.count = count;
+//    }
+//
+//    public static class AdapterCDATA extends XmlAdapter<String, String> {
+//
+//        @Override
+//        public String marshal(String arg0) throws Exception {
+//            if (arg0.isEmpty()) return "";
+//            return "<![CDATA[" + arg0 + "]]>";
+//        }
+//
+//        @Override
+//        public String unmarshal(String arg0) throws Exception {
+//            return arg0;
+//        }
+//
+//    }
 }
--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
@@ -2,9 +2,9 @@ package com.javarush.task.task33.task3309;
 
 //import com.javarush.task.task33.task3308.Shop;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
+//import javax.xml.bind.JAXBContext;
+//import javax.xml.bind.JAXBException;
+//import javax.xml.bind.Marshaller;
 import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.List;
@@ -41,50 +41,50 @@ toXmlWithComment(firstSecondObject, "second", "it's a comment")
 3. Если во входящем xml отсутствует искомый тег, то добавлять комментарии не нужно.
 */
 public class Solution {
-    public static String toXmlWithComment(Object obj, String tagName, String comment) throws
-            JAXBException {
-        StringWriter writer = new StringWriter();
-        JAXBContext context = JAXBContext.newInstance(obj.getClass());
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        marshaller.marshal(obj, writer);
-
-        StringBuilder sb = new StringBuilder();
-        String reg = "<" + tagName;
-        String coment = "<!--" + comment + "-->\n";
-        List<String> xml = Arrays.asList(writer.toString().split("\n"));
-        sb.append(xml.get(0).replace("standalone=\"yes\"", "standalone=\"no\"")+"\n");
-        for (int i = 1; i < xml.size(); i++) {
-            String ss = xml.get(i);
-
-            int f = ss.indexOf(reg);
-            if (f >= 0) {
-                sb.append(ss, 0, f).append(coment);
-            }
-
-            int n = ss.indexOf(">");
-            int k = ss.lastIndexOf("<");
-            String sd = "";
-            if (k > n) sd = ss.substring(n + 1, k);
-
-            if (!sd.equals("")) {
-                if (sd.matches((".*(&amp;|&lt;|&gt;|'|\").*"))) {
-                    sd = sd.replaceAll("&amp;", "&");
-                    sd = sd.replaceAll("&lt;", "<");
-                    sd = sd.replaceAll("&gt;", ">");
-                    sd = "<![CDATA[" + sd + "]]>";
-                    ss = ss.substring(0, n + 1) + sd + ss.substring(k);
-                }
-            }
-            sb.append(ss).append("\n");
-        }
-        return sb.toString();
-    }
-
-    public static void main(String[] args) throws JAXBException {
-//        Shop shop = new Shop();
-//        shop.count = 1;
-//        shop.secretData = new String[]{"secret1", "<secretData>", ""};
-//        System.out.println(toXmlWithComment(shop, "secretData", "comment"));
-    }
+//    public static String toXmlWithComment(Object obj, String tagName, String comment) throws
+//            JAXBException {
+//        StringWriter writer = new StringWriter();
+//        JAXBContext context = JAXBContext.newInstance(obj.getClass());
+//        Marshaller marshaller = context.createMarshaller();
+//        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+//        marshaller.marshal(obj, writer);
+//
+//        StringBuilder sb = new StringBuilder();
+//        String reg = "<" + tagName;
+//        String coment = "<!--" + comment + "-->\n";
+//        List<String> xml = Arrays.asList(writer.toString().split("\n"));
+//        sb.append(xml.get(0).replace("standalone=\"yes\"", "standalone=\"no\"")+"\n");
+//        for (int i = 1; i < xml.size(); i++) {
+//            String ss = xml.get(i);
+//
+//            int f = ss.indexOf(reg);
+//            if (f >= 0) {
+//                sb.append(ss, 0, f).append(coment);
+//            }
+//
+//            int n = ss.indexOf(">");
+//            int k = ss.lastIndexOf("<");
+//            String sd = "";
+//            if (k > n) sd = ss.substring(n + 1, k);
+//
+//            if (!sd.equals("")) {
+//                if (sd.matches((".*(&amp;|&lt;|&gt;|'|\").*"))) {
+//                    sd = sd.replaceAll("&amp;", "&");
+//                    sd = sd.replaceAll("&lt;", "<");
+//                    sd = sd.replaceAll("&gt;", ">");
+//                    sd = "<![CDATA[" + sd + "]]>";
+//                    ss = ss.substring(0, n + 1) + sd + ss.substring(k);
+//                }
+//            }
+//            sb.append(ss).append("\n");
+//        }
+//        return sb.toString();
+//    }
+//
+//    public static void main(String[] args) throws JAXBException {
+////        Shop shop = new Shop();
+////        shop.count = 1;
+////        shop.secretData = new String[]{"secret1", "<secretData>", ""};
+////        System.out.println(toXmlWithComment(shop, "secretData", "comment"));
+//    }
 }
