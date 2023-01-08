3eb77debc739ad5a220360900f471142ec4ff0ec
Alex Plekhov <aplekhov@wiley.com>
2023-01-08 20:15:26 +0300

    Add more tasks for generics wildcards

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task35/task3512/Generator.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task35/task3512/Generator.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task35.task3512;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class Generator<T> {
     private final Class<T> clazz;
 
@@ -9,8 +11,9 @@ public class Generator<T> {
 
     T newInstance() {
         try {
-            return clazz.newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
+            return clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException |
+                 InvocationTargetException e) {
             throw new RuntimeException(e);
         }
     }
